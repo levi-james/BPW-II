ac24137c15d334255bfca9e9795fec59bbfbafb5
levi-james <levivermaning@gmail.com>
2021-03-04 16:16:40 +0100

    first (very late) commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    PlayerStates states;
+    public float smolSpeed;
+    public float bigSpeed;
+    public float medSpeed; 
+    public float speed = 12f;
+
+    public CharacterController controller;
+
+    Transform playerObj;
+
+    public GameObject BigVision;
+    public GameObject MediumVision;
+    public GameObject SmolVision;
+
+    [SerializeField] GameObject childPos;
+    GameObject slimeChild;
+    bool slimeChildCreated = false;
+    int slimeChildrenInScene = 0;
+
+    [SerializeField] AudioSource chomp;
+    void Update()
+    {
+        states = GetComponent<PlayerStates>();
+        playerObj = GameObject.Find("Player").GetComponent<Transform>();
+        slimeChild = GameObject.Find("SlimeChild");
+        
+    }
+
+    public void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+
+    public void CreateSlimeChild()
+    {
+        if(Input.GetKeyDown(KeyCode.X) && !slimeChildCreated)
+        {
+            Instantiate(slimeChild, childPos.transform.position, Quaternion.identity);
+            slimeChildCreated = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Finish"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        if (other.CompareTag("LilFella"))
+        {
+            chomp.Play();
+            Destroy(other.gameObject);
+            states.currentState = PlayerStates.PlayerMonsterStates.Smol;
+
+        }
+
+        if (other.CompareTag("MediumFella"))
+        {
+            Destroy(other.gameObject);
+            states.currentState = PlayerStates.PlayerMonsterStates.Medium;
+        }
+
+        if (other.CompareTag("BigFella"))
+        {
+            states.currentState = PlayerStates.PlayerMonsterStates.Big;
+        }
+
+
+    }
+
+    public void Smol()
+    {
+        playerObj.transform.localScale = new Vector3(1, 0.2f, 1);
+        SmolVision.SetActive(true);
+        MediumVision.SetActive(false);
+        BigVision.SetActive(false);
+        speed = smolSpeed;
+
+    }
+
+    public void Medium()
+    {
+        playerObj.transform.localScale = new Vector3(1, 1f, 1);
+        SmolVision.SetActive(false);
+        MediumVision.SetActive(true);
+        BigVision.SetActive(false);
+        speed = medSpeed;
+    }
+
+    public void Big()
+    {
+        playerObj.transform.localScale = new Vector3(1, 1.5f, 1);
+        SmolVision.SetActive(false);
+        MediumVision.SetActive(false);
+        BigVision.SetActive(true);
+        speed = bigSpeed;
+    }
+
+
+    public void DebugMonsters()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerStates.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStates : MonoBehaviour
+{
+    Player player;
+    public enum PlayerMonsterStates
+    {
+        Big,
+        Medium,
+        Smol,
+        Dead,
+        Cutscene
+    }
+
+    public PlayerMonsterStates currentState;
+
+    void Start()
+    {
+        player = GetComponent<Player>();
+        //currentState = PlayerMonsterStates.Medium;
+    }
+
+    void Update()
+    {
+        switch(currentState)
+        {
+            case PlayerMonsterStates.Big:
+                player.Move();
+                player.Big();
+                Debug.Log("Big monster time");
+                break;
+            case PlayerMonsterStates.Medium:
+                player.Move();
+                player.Medium();
+                player.CreateSlimeChild();
+                Debug.Log("Medium monster time");
+                break;
+            case PlayerMonsterStates.Smol:
+                player.Smol();
+                player.Move();
+                Debug.Log("Smol monster time");
+                break;
+            case PlayerMonsterStates.Dead:
+                Debug.Log("u died");
+                break;
+            case PlayerMonsterStates.Cutscene:
+                Debug.Log("cutscene now");
+                break;
+        }
+    }
+}
