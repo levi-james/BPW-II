b2a509fa5d1c46555460a29bda69f3258516da71
levi-james <levivermaning@gmail.com>
2021-03-08 12:18:33 +0100

    updated levels, added new sprites

---
+++ Assets/CameraShake.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    //h ttps://www.youtube.com/watch?v=9A9yj8KnM8c
+
+    public IEnumerator CamShake(float duration, float magnitude)
+    {
+        Vector3 originalPos = transform.localPosition;
+
+        float elapsed = 0f;
+        
+        while(elapsed < duration)
+        {
+            float x = Random.Range(-1, 1) * magnitude;
+            float y = Random.Range(-1, 1) * magnitude;
+
+            transform.localPosition = new Vector3(x, y, originalPos.z);
+
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.localPosition = originalPos;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,31 +7,33 @@ using UnityEngine.SceneManagement;
 public class Player : MonoBehaviour
 {
     PlayerStates states;
-    public float smolSpeed;
-    public float bigSpeed;
-    public float medSpeed; 
-    public float speed = 12f;
+    [SerializeField] float smolSpeed;
+    [SerializeField] float bigSpeed;
+    [SerializeField] float medSpeed;
+    [SerializeField] float speed;
+
+    [SerializeField] private float pushPower = 5f;
 
     public CharacterController controller;
 
     Transform playerObj;
 
-    public GameObject BigVision;
-    public GameObject MediumVision;
-    public GameObject SmolVision;
+    [SerializeField] GameObject BigVision;
+    [SerializeField] GameObject MediumVision;
+    [SerializeField] GameObject SmolVision;
 
     [SerializeField] GameObject childPos;
     GameObject slimeChild;
-    bool slimeChildCreated = false;
-    int slimeChildrenInScene = 0;
+    private int slimeChildrenInScene = 0;
 
     [SerializeField] AudioSource chomp;
+
     void Update()
     {
         states = GetComponent<PlayerStates>();
         playerObj = GameObject.Find("Player").GetComponent<Transform>();
         slimeChild = GameObject.Find("SlimeChild");
-        
+
     }
 
     public void Move()
@@ -46,19 +48,23 @@ public class Player : MonoBehaviour
 
     public void CreateSlimeChild()
     {
-        if(Input.GetKeyDown(KeyCode.X) && !slimeChildCreated)
+        if (Input.GetKeyDown(KeyCode.X) && slimeChildrenInScene < 2)
         {
             Instantiate(slimeChild, childPos.transform.position, Quaternion.identity);
-            slimeChildCreated = true;
+            slimeChildrenInScene++;
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        //finishing a level teleports you to the next
         if(other.CompareTag("Finish"))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
+
+
+
         if (other.CompareTag("LilFella"))
         {
             chomp.Play();
@@ -67,17 +73,45 @@ public class Player : MonoBehaviour
 
         }
 
-        if (other.CompareTag("MediumFella"))
+        if (other.CompareTag("MediumFella") && states.currentState == PlayerStates.PlayerMonsterStates.Big)
         {
             Destroy(other.gameObject);
             states.currentState = PlayerStates.PlayerMonsterStates.Medium;
         }
 
-        if (other.CompareTag("BigFella"))
+        if (other.CompareTag("BigFella") && states.currentState == PlayerStates.PlayerMonsterStates.Medium)
         {
             states.currentState = PlayerStates.PlayerMonsterStates.Big;
+            Destroy(other.gameObject);
+        }
+    }
+
+    //ht tps://docs.unity3d.com/ScriptReference/CharacterController.OnControllerColliderHit.html?_ga=2.251409286.1855934843.1615121525-522815912.1603372675
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Rigidbody body = hit.collider.attachedRigidbody;
+
+        // no rigidbody
+        if (body == null || body.isKinematic)
+        {
+            return;
         }
 
+        // We dont want to push objects below us
+        if (hit.moveDirection.y < -0.3)
+        {
+            return;
+        }
+
+        // Calculate push direction from move direction,
+        // we only push objects to the sides never up and down
+        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+
+        // If you know how fast your character is trying to move,
+        // then you can also multiply the push velocity by that.
+
+        // Apply the push
+        body.velocity = pushDir * pushPower;
 
     }
 
@@ -107,6 +141,7 @@ public class Player : MonoBehaviour
         MediumVision.SetActive(false);
         BigVision.SetActive(true);
         speed = bigSpeed;
+
     }
 
 
