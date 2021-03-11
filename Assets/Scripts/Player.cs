6760b2661f55c52edb2aa90405f8e7c8d1549bc3
levi-james <levivermaning@gmail.com>
2021-03-11 16:35:12 +0100

    levels added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Text;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -11,6 +12,7 @@ public class Player : MonoBehaviour
     [SerializeField] float bigSpeed;
     [SerializeField] float medSpeed;
     [SerializeField] float speed;
+    public Camera mainCam;
 
     [SerializeField] private float pushPower = 5f;
 
@@ -28,12 +30,22 @@ public class Player : MonoBehaviour
 
     [SerializeField] AudioSource chomp;
 
-    void Update()
+    public Animator smokeAnim;
+
+    GameObject winCanvas;
+
+    private void Start()
     {
         states = GetComponent<PlayerStates>();
         playerObj = GameObject.Find("Player").GetComponent<Transform>();
         slimeChild = GameObject.Find("SlimeChild");
+        winCanvas = GameObject.Find("WinCanvas");
 
+        winCanvas.SetActive(false);
+    }
+    void Update()
+    {
+        Restart();
     }
 
     public void Move()
@@ -45,10 +57,10 @@ public class Player : MonoBehaviour
 
         controller.Move(move * speed * Time.deltaTime);
     }
-
+    
     public void CreateSlimeChild()
     {
-        if (Input.GetKeyDown(KeyCode.X) && slimeChildrenInScene < 2)
+        if (Input.GetMouseButtonDown(0) && slimeChildrenInScene < 1)
         {
             Instantiate(slimeChild, childPos.transform.position, Quaternion.identity);
             slimeChildrenInScene++;
@@ -57,13 +69,13 @@ public class Player : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        //finishing a level teleports you to the next
+        //finishing a level teleports you to the next one
         if(other.CompareTag("Finish"))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            states.currentState = PlayerStates.PlayerMonsterStates.Cutscene;
+            winCanvas.SetActive(true);
+            StartCoroutine(GoToNextLevel());
         }
-
-
 
         if (other.CompareTag("LilFella"))
         {
@@ -76,6 +88,7 @@ public class Player : MonoBehaviour
         if (other.CompareTag("MediumFella") && states.currentState == PlayerStates.PlayerMonsterStates.Big)
         {
             Destroy(other.gameObject);
+            chomp.Play();
             states.currentState = PlayerStates.PlayerMonsterStates.Medium;
         }
 
@@ -84,40 +97,75 @@ public class Player : MonoBehaviour
             states.currentState = PlayerStates.PlayerMonsterStates.Big;
             Destroy(other.gameObject);
         }
+
+        if (other.CompareTag("BigFella") && states.currentState == PlayerStates.PlayerMonsterStates.Smol)
+        {
+            states.currentState = PlayerStates.PlayerMonsterStates.Dead;
+        }
     }
 
     //ht tps://docs.unity3d.com/ScriptReference/CharacterController.OnControllerColliderHit.html?_ga=2.251409286.1855934843.1615121525-522815912.1603372675
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        Rigidbody body = hit.collider.attachedRigidbody;
-
-        // no rigidbody
-        if (body == null || body.isKinematic)
+        if(hit.collider.CompareTag("MovableBox") && states.currentState == PlayerStates.PlayerMonsterStates.Big)
         {
-            return;
-        }
+            Rigidbody body = hit.collider.attachedRigidbody;
 
-        // We dont want to push objects below us
-        if (hit.moveDirection.y < -0.3)
+            // no rigidbody
+            if (body == null || body.isKinematic)
+            {
+                return;
+            }
+
+            // We dont want to push objects below us
+            if (hit.moveDirection.y < -0.3)
+            {
+                return;
+            }
+
+            // Calculate push direction from move direction,
+            // we only push objects to the sides never up and down
+            Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+
+            // If you know how fast your character is trying to move,
+            // then you can also multiply the push velocity by that.
+
+            // Apply the push
+            body.velocity = pushDir * pushPower;
+        }
+        else if(hit.collider.CompareTag("SlimeChild") && states.currentState == PlayerStates.PlayerMonsterStates.Medium)
         {
-            return;
+            Rigidbody body = hit.collider.attachedRigidbody;
+
+            // no rigidbody
+            if (body == null || body.isKinematic)
+            {
+                return;
+            }
+
+            // We dont want to push objects below us
+            if (hit.moveDirection.y < -0.3)
+            {
+                return;
+            }
+
+            // Calculate push direction from move direction,
+            // we only push objects to the sides never up and down
+            Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
+
+            // If you know how fast your character is trying to move,
+            // then you can also multiply the push velocity by that.
+
+            // Apply the push
+            body.velocity = pushDir * pushPower;
         }
-
-        // Calculate push direction from move direction,
-        // we only push objects to the sides never up and down
-        Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, hit.moveDirection.z);
-
-        // If you know how fast your character is trying to move,
-        // then you can also multiply the push velocity by that.
-
-        // Apply the push
-        body.velocity = pushDir * pushPower;
-
     }
 
     public void Smol()
     {
-        playerObj.transform.localScale = new Vector3(1, 0.2f, 1);
+        playerObj.transform.localScale = new Vector3(1, 0.5f, 1);
+        playerObj.transform.position = new Vector3(transform.position.x, transform.position.y / 5, transform.position.z);
+        //transform.localPosition = new Vector3(transform.position.x, 0.5f, transform.position.z);
         SmolVision.SetActive(true);
         MediumVision.SetActive(false);
         BigVision.SetActive(false);
@@ -127,7 +175,8 @@ public class Player : MonoBehaviour
 
     public void Medium()
     {
-        playerObj.transform.localScale = new Vector3(1, 1f, 1);
+        playerObj.transform.localScale = new Vector3(2, 3, 2);
+        playerObj.transform.position = new Vector3(transform.position.x, transform.position.y / 2, transform.position.z);
         SmolVision.SetActive(false);
         MediumVision.SetActive(true);
         BigVision.SetActive(false);
@@ -136,7 +185,8 @@ public class Player : MonoBehaviour
 
     public void Big()
     {
-        playerObj.transform.localScale = new Vector3(1, 1.5f, 1);
+        playerObj.transform.localScale = new Vector3(2, 4, 2);
+       playerObj.transform.position = new Vector3(transform.position.x, 1.75f, transform.position.z);
         SmolVision.SetActive(false);
         MediumVision.SetActive(false);
         BigVision.SetActive(true);
@@ -144,9 +194,22 @@ public class Player : MonoBehaviour
 
     }
 
-
-    public void DebugMonsters()
+    public void Dead()
     {
         
+        smokeAnim.SetBool("Dead", true);
+    }
+    public IEnumerator GoToNextLevel()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+    public void Restart()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            Scene scene = SceneManager.GetActiveScene(); SceneManager.LoadScene(scene.name);
+        }
     }
 }
---
+++ Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    [SerializeField] GameObject door;
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("MovableBox") || other.CompareTag("SlimeChild") || other.CompareTag("Player"))
+        {
+            door.SetActive(false);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        door.SetActive(true);
+    }
+}
